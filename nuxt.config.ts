c67cb87a9f0682d2ee2c996d72938b7d1757efce
Hassan Alkhalidi <gruceing@gmail.com>
2022-07-11 16:41:08 +0300

    qwd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,14 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     modules: [
+        // UnoCSS
         '@unocss/nuxt',
+
+        // Color Mode
         "@nuxtjs/color-mode",
-        // '@nuxtjs/tailwindcss'
+
+        // Lodash
+        "nuxt-lodash",
     ],
 
     colorMode: {
