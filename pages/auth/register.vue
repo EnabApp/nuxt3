e008d7bfe7f7c4b759e46c51112cea2a32f54ee6
hassan_000 <salem.cf11@gmail.com>
2022-07-19 21:45:30 +0300

    Update register.vue

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -9,7 +9,7 @@
                 <UiInput icon="i-clarity-user-solid dark:text-gray-400" label="الاسم" placeholder="ادخل الاسم الكامل" type="text" />
                 <UiInput icon="i-clarity-email-solid dark:text-gray-400" label="البريد الالكتروني" placeholder="ادخل بريدالكتروني غير مستخدم مسبقاً" type="text" />
                 <UiInput icon="i-clarity-lock-solid dark:text-gray-400" label="كلمة السر " placeholder="ادخل كلمة السر" type="password" />
-                <UiInput icon="i-clarity-lock-solid dark:text-gray-400" label="تأكيد كلمة السر" placeholder="ادخل كلمة مجدداً" type="password" />
+                <UiInput icon="i-clarity-lock-solid dark:text-gray-400" label="تأكيد كلمة السر" placeholder="ادخل كلمة السر مجدداً" type="password" />
                 <div flex="~ col" >
                 <button bg="primary" dark:bg-gradient="to-l from-primary to-b-60" class="py-2 text-lg text-white border-none rounded-lg cursor-pointer">
                     تسجيل 
