18e7929d86b7896377c17515798942d760c099a9
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-07-26 14:18:17 +0300

    tauri push

--- components/BottomBar/Menu.vue
+++ components/BottomBar/Menu.vue
@@ -25,26 +25,7 @@
                         <!-- Bottom -->
                         <div flex="~" items="center" justify="between" p="y-2 x-20" border="rounded-b-lg" bg="w-10">
                             <div>
-                                <div>
-                                    <div class="text-2xl i-fluent:power-20-regular text-w-100">
-                                        <button @click="
-                                            () => {
-                                                powerToggle(), (menuState = false);
-                                            }
-                                        " class="w-10 h-10 rounded-full"></button>
-                                    </div>
-                                </div>
-
-                                <div class="relative" v-if="powerState">
-                                    <Transition>
-                                        <div class="absolute backdrop-blur-lg bottom-15 left-135" border="rounded-xl" ref="target" bg="b-60" p="4" w="48" h="content" position="fixed" flex="~ col gap-2" z="250">
-                                            <slot name="menu" />
-
-                                            <UiMenuItem icon="i-akar-icons-copy" title="hjg" />
-                                            <UiMenuItem icon="i-akar-icons-clipboard" title="gjg" />
-                                        </div>
-                                    </Transition>
-                                </div>
+                                <div v-if="getName()" @click="closeApp" cursor="pointer" class="text-2xl i-fluent:power-20-regular text-w-100"></div>
                             </div>
 
                             <div>
@@ -79,11 +60,13 @@
 </template>
 
 <script setup>
+import { exit } from '@tauri-apps/api/process';
+import { getName } from '@tauri-apps/api/app';
+
 import { useNow, useDateFormat } from "@vueuse/core";
 
 const appsStore = useStoreApps();
 const [menuState, menuToggle] = useToggle(false);
-const [powerState, powerToggle] = useToggle(false);
 const [state, toggle] = useToggle(false);
 const formattedDate = useDateFormat(useNow(), "YYYY-MM-DD");
 const formattedTime = useDateFormat(useNow(), "HH:mm:ss");
@@ -95,6 +78,10 @@ onKeyStroke(['Meta'], (e) => {
   e.preventDefault();
   e.stopPropagation();
 })
+
+
+const closeApp = async () => await exit(1);
+
 </script>
 
 <style scoped>
