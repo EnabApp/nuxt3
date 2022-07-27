2961437a9a3d5079aeed8b967d233315ea663e2a
Montazer Malik <mntthr1122@gmail.com>
2022-07-27 22:15:29 +0300

    Update ServicesStore.vue

--- components/App/ServicesStore.vue
+++ components/App/ServicesStore.vue
@@ -7,31 +7,62 @@
         v-show="!app.minimized"
         :app="app"
       >
-        <div m="4" flex="~ col grow gap-4">
-          <UiInput v-model="serviceTitle" placeholder="خدمة جديدة" @keypress.enter="newService()" />
+        <div m="4" flex="~ col grow gap-4" h="md">
+          <!-- Search and Points -->
+          <div flex="~ row" justify="between">
+            <UiInput class="basis-2/6" placeholder="البحث عن خدمة" @keypress.enter="newService()" /> 
+            <div class="place-items-left">
+              <div class="i-line-md:question-circle-twotone text-warning animate-pulse" cursor="pointer"  w="5" h="5"></div>
+              <nuxt-link to="/" class="no-underline">
+                <span m="2" class="basis-1/6" flex="~" text="2xl success-500">نقاطي : 499</span>
+              </nuxt-link>
+            </div>
+          </div>
 
           <UiTabGroup :col="false" :tabs="['التصنيفات','جميع الخدمات', 'خدماتي']">
+            <!-- BUSSINESES -->
             <template #tab-1>
-              <div max-h="2xl lg:xl md:xl" m="y-8" grid="~ cols-2 gap-2" p="l-2" border="rounded-lg" overflow-y="scroll">
-                <div v-for="service in 4" :key="service" bg="w-10" items="center" p="4" border="rounded-lg" un-text="w-50">
-                  <div class="i-bi-check-circle-fill hover:text-w-70" w="15" h="15"></div>
-                  <span self="auto" text="2xl" m="4">service</span>
+              <div p="4" h="80" grid="~ cols-4 gap-2" border="rounded-lg" overflow-y="scroll">
+                <!-- BUSSINESES I Own -->
+                <div @click="clicked(-1)" grid="~ flow-row auto-rows-max" cursor="pointer" bg="w-10" place="items-center" p="4" border="rounded-lg 2" :class="false?'animate-pulse border-errors-700' :'transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-100 border-success-500'" >
+                  <div class="i-ic:baseline-local-grocery-store" w="15" h="15"></div>
+                  <span self="auto" text="2xl" m="4">سوبرماركت</span>
+                  <span v-if="false" self="auto" text="2xl">free</span>
+                </div>
+                <!-- BUSSINESES I don't have -->
+                <div @click="clicked(key)" v-for="(bussiness,key) in 4" :key="bussiness" grid="~ flow-row auto-rows-max" cursor="help" bg="w-10" place="items-center" p="4" border="rounded-lg 2" :class="key=1 ?'animate-pulse border-error-500' :'transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-100 border-success-500'" >
+                  <div class="i-fluent:form-new-24-filled" w="15" h="15"></div>
+                  <span self="auto" text="2xl" m="4">New</span>
+                  <span self="auto" text="2xl" :class="false ? 'text-success-500':'text-error-500'">1999 نقطة</span>
                 </div>
               </div>
             </template>
+            <!-- All Services -->
             <template #tab-2>
-                <div max-h="2xl lg:xl md:xl" m="y-8" grid="~ lg:cols-3 cols-2 gap-2" p="l-2" border="rounded-lg" overflow-y="scroll">
-                  <div v-for="service in 10" :key="service" bg="w-10" items="center" p="4" border="rounded-lg" un-text="w-50">
-                    <div class="i-bi-check-circle-fill hover:text-w-70" w="15" h="15"></div>
-                    <span self="auto" text="2xl" m="4">title</span>
-                  </div>
+              <div p="4" h="80" grid="~ lg:cols-4 cols-2 gap-2" border="rounded-lg" overflow-y="auto">
+                <!-- Services I Own -->
+                <div @click="clicked(1)" grid="~ flow-row auto-rows-max" cursor="pointer" bg="w-10" place="items-center" p="4" border="rounded-lg 2" :class="false?'animate-pulse border-errors-700' :'transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-100 border-success-500'" >
+                  <div class="i-bi-check-circle-fill" w="15" h="15"></div>
+                  <span self="auto" text="2xl" m="4">اسم الخدمة</span>
+                  <span v-if="false" self="auto" text="2xl">30 نقطة</span>
                 </div>
+                <!-- Services I don't have -->
+                <div @click="clicked(key)" v-for="(service,key) in 4" :key="service" grid="~ flow-row auto-rows-max" bg="w-10" place="items-center" p="4" border="rounded-lg 2" :class="true?'animate-pulse border-error-500 cursor-pointer' :'transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-100 border-success-500'" >
+                  <div class="i-ic:baseline-local-grocery-store" w="15" h="15"></div>
+                  <span self="auto" text="2xl" m="4">New</span>
+                  <span self="auto" text="2xl" :class="true ? 'text-success-500':'text-error-500'">400 نقطة</span>
+                </div>
+              </div>
             </template>
+            <!-- My Services -->
             <template #tab-3>
-              <h1>About</h1>
-            </template>
-            <template #tab-4>
-              <h1>Contact</h1>
+              <div p="4" h="80" grid="~ lg:cols-4 cols-2 gap-2" border="rounded-lg" overflow-y="auto">
+                <div @click="clicked(key)" v-for="(service,key) in 6" :key="service" grid="~ flow-row auto-rows-max" cursor="pointer" bg="w-10" place="items-center" p="4" border="rounded-lg 2" :class="false?'animate-pulse border-errors-700' :'transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-100 border-success-500'" >
+                  <div class="i-bi-check-circle-fill" w="15" h="15"></div>
+                  <span self="auto" text="2xl" m="4">اسم الخدمة</span>
+                  <span v-if="false" self="auto" text="2xl">30 نقطة</span>
+                </div>
+              </div>
             </template>
           </UiTabGroup>
         </div>
@@ -71,7 +102,9 @@ const newService = () => {
   })
   serviceTitle.value = ''
 };
-
+const clicked = (key) =>{
+  console.log(key)
+}
 </script>
 
 <style scoped>
