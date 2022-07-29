07d2265f5dc2a7c6f13c0bb6eef8bc64e05ba70b
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-07-29 22:29:00 +0300

    Update Supermarket.vue

--- components/App/Supermarket.vue
+++ components/App/Supermarket.vue
@@ -2,19 +2,18 @@
   <!-- Application -->
   <Transition>
     <UiDesktopWindow v-if="app.running" v-show="!app.minimized" :app="app">
-    <UiTabGroup flex="~ col" p="2" h="full" :tabs="[
-    'الصفحة الرئيسية', 'التحكم', 'Contact']">
-      <template #tab-1>
-        <AppSupermarketTabsHome :app="app" />
-      </template>
-      <template #tab-2>
-        <AppSupermarketTabsControl :app="app" />
-      </template>
-      <template #tab-3>
-        <h1>Contact</h1>
-      </template>
-    </UiTabGroup>
-  </UiDesktopWindow>
+      <UiTabGroup flex="~ col" p="2" h="full" :tabs="['الصفحة الرئيسية', 'التحكم', 'Contact']">
+        <template #tab-1>
+          <AppSupermarketTabsHome :app="app" />
+        </template>
+        <template #tab-2>
+          <AppSupermarketTabsControl :app="app" />
+        </template>
+        <template #tab-3>
+          <h1>Contact</h1>
+        </template>
+      </UiTabGroup>
+    </UiDesktopWindow>
   </Transition>
 </template>
 
