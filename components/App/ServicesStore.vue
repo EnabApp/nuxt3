32052b9ab95f29075b02df2efb05e41f68331f08
Montazer Malik <mntthr1122@gmail.com>
2022-07-29 21:01:51 +0300

    dd

--- components/App/ServicesStore.vue
+++ components/App/ServicesStore.vue
@@ -7,15 +7,13 @@
         v-show="!app.minimized"
         :app="app"
       >
-        <div m="4" flex="~ col grow gap-4" h="md">
+        <div m="2" flex="~ col grow gap-4">
           <!-- Search and Points -->
-          <div flex="~ row" justify="between">
+          <div flex="~" justify="between">
             <UiInput class="basis-2/6" placeholder="البحث عن خدمة" @keypress.enter="newService()" /> 
-            <div class="place-items-left">
-              <div class="i-line-md:question-circle-twotone text-warning animate-pulse" cursor="pointer"  w="5" h="5"></div>
-              <nuxt-link to="/" class="no-underline">
-                <span m="2" class="basis-1/6" flex="~" text="2xl success-500">نقاطي : 499</span>
-              </nuxt-link>
+            <div>
+              <span class="basis-1/6" text="2xl success-500">نقاطي : 499</span>
+              <div @click="pageToggle()" class="i-line-md:question-circle-twotone text-warning animate-pulse" cursor="pointer"  w="5" h="5"></div>
             </div>
           </div>
 
@@ -26,14 +24,23 @@
             </template>
             <!-- All Services -->
             <template #tab-2>
-              <AppServicesStoreTabsServices />
+              <AppServicesStoreTabsAllServices />
             </template>
             <!-- My Services -->
             <template #tab-3>
               <AppServicesStoreTabsMyServices />
             </template>
           </UiTabGroup>
+
         </div>
+          <Transition name="close-transition">
+            <div v-if="isOpen" absolute="~" bg="w-90" place="items-center" p="4" border="rounded-lg 2 w-10" w="full" h="full">
+              <div flex="~ row" justify="between">
+                <span self="auto" text="2xl" m="4">Page Name</span>
+                <div @click="pageToggle()" class="i-ep:close-bold" w="15" h="15" m="3"></div>
+              </div>
+            </div>
+          </Transition>
       </UiDesktopWindow>
     </Transition>
   </ContextMenu>
@@ -59,20 +66,12 @@ AppsStore.register(myApp);
 const app = computed(() => AppsStore.getApp(myApp));
 
 ///////////////////////////////
-const serviceTitle = ref('');
-const services = ref([]);
+const { isOpen, open } = useServicesStore()
 
-const newService = () => {
-  services.value.push({
-    title: serviceTitle.value,
-    completed: false,
-    time: useDateFormat(useNow(), "YYYY-MM-DD"),
-  })
-  serviceTitle.value = ''
-};
-const clicked = (key) =>{
-  console.log(key)
+const pageToggle = () =>{
+  console.log(isOpen)
 }
+
 </script>
 
 <style scoped>
--- components/App/ServicesStore/Tabs/MyServices.vue
+++ components/App/ServicesStore/Tabs/MyServices.vue
@@ -1,6 +1,6 @@
 <template>
-  <div p="4" h="80" grid="~ lg:cols-4 cols-2 gap-2" border="rounded-lg" overflow-y="auto">
-    <div @click="clicked(key)" v-for="(service,key) in 6" :key="service" grid="~ flow-row auto-rows-max" cursor="pointer" bg="w-10" place="items-center" p="4" border="rounded-lg 2" :class="false?'animate-pulse border-errors-700' :'transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-100 border-success-500'" >
+  <div p="2" h="96" grid="~ lg:cols-4 cols-2 gap-2" border="rounded-lg" overflow-y="auto">
+    <div v-for="service in 4" :key="service" grid="~ flow-row auto-rows-max" cursor="pointer" bg="w-10" place="items-center" p="4" border="rounded-lg 2" class="transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-100 border-success-500" >
         <div class="i-bi-check-circle-fill" w="15" h="15"></div>
         <span self="auto" text="2xl" m="4">اسم الخدمة</span>
         <span v-if="false" self="auto" text="2xl">30 نقطة</span>
