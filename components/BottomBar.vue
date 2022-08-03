b95eba2555144cb76429553dc7ac179635e2cf45
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-08-03 17:21:12 +0300

    ضصي

--- components/BottomBar.vue
+++ components/BottomBar.vue
@@ -14,10 +14,7 @@
         <div h="32px" w="1px" bg="secondaryOp" m="x-16px"></div>
 
         <!-- Search -->
-        <div ref="searchEl" h="43px" flex="~ gap-2" p="x-13px" items="center" justify="center" bg="secondaryOp" un-text="secondary" border="rounded-10px">
-          <div h="16px" w="16px" class="i-fa-search"></div>
-          <input placeholder="بحث..." type="search" un-text="lg white" class="bg-transparent border-0 outline-none focus:outline-none" />
-        </div>
+        <BottomBarSearch />
 
         <!-- Divider -->
         <div h="32px" w="1px" bg="secondaryOp" m="x-16px"></div>
@@ -40,7 +37,7 @@
         <!-- Divider -->
         <div h="32px" w="1px" bg="secondaryOp" m="x-16px"></div>
 
-        <div flex="~ gap-24px" items="center" un-text="white" m="l-8px">
+        <div flex="~ gap-24px" items="center" bg="hover:secondaryOp" p="y-2 x-4" border="rounded-10px" cursor="pointer" un-text="white">
             <!-- Calendar -->
             <div h="32px" w="32px" class="i-bxs-calendar"></div>
 
@@ -53,16 +50,16 @@
         
         <!-- Bottombar minimize -->
         <div un-text="white">
-            <div cursor="pointer" @click="barToggle()" h="32px" w="32px" class="i-bi-arrow-down-right-square-fill"></div>
+            <div cursor="pointer" @dblclick="barToggle()" h="32px" w="32px" class="i-bi-arrow-down-right-square-fill"></div>
         </div>
       </div>
 
     </div>
 
     <!-- Floating Button -->
-    <div ref="floatingButton" p="20px" position="absolute" left="0" bottom="0" :style="style" z="200" v-else>
+    <div ref="floatingButton" v-else p="20px" position="fixed" left="0" bottom="0" :style="style" z="200">
       <div un-text="white" bg="secondaryOp" border="rounded-10px" w="60px" h="60px" flex="~" items="center" justify="center">
-        <div @click="barToggle()" cursor="pointer" h="32px" w="32px" class="i-bi-arrow-up-right-square-fill"></div>
+        <div @dblclick="barToggle()" cursor="pointer" h="32px" w="32px" class="i-bi-arrow-up-right-square-fill"></div>
       </div>
     </div>
 </template>
@@ -73,15 +70,18 @@ const date = computed(() => today.toLocaleDateString("ar-IQ"))
 
 const appsStore = useStoreApps()
 
-const online = useOnline()
+const { width, height } = useWindowSize()
 
 const [barState, barToggle] = useToggle(false)
-
-
 const floatingButton = ref(null)
 
-const { x, y, style } = useDraggable(floatingButton, {
-  // onEnd: () => console.log('hi')
+const { x, y, style, position } = useDraggable(floatingButton, {
+  initialValue: { x: 0, y: '100%' },
+  stopPropagation: true,
+  onEnd: () => {
+    y.value = y.value > height.value/2 || y.value == '100%' ? '100%' : 0
+    x.value = x.value > width.value/2 ? width.value - 100 : 0
+  }
 })
 
 
---
+++ components/BottomBar/Search.vue
@@ -0,0 +1,33 @@
+<template>
+    <div @click="searchPanelToggle" ref="searchEl" relative h="43px" flex="~ gap-2" p="x-13px" items="center" justify="center" bg="secondaryOp" un-text="secondary focus-within:white" border="rounded-10px">
+        <div h="16px" w="16px" class="i-fa-search"></div>
+        <input placeholder="بحث..." type="search" un-text="lg secondary focus:white" class="bg-transparent border-0 outline-none focus:outline-none" />
+        <Transition name="search-panel">
+            <div v-if="searchPanelState" position="absolute" w="full" p="4" bottom="15" bg="secondaryOp" border="rounded-10px">
+                <div class="i-line-md-loading-loop"></div>
+            </div>
+        </Transition>
+    </div>
+</template>
+
+<script setup>
+    const [searchPanelState, searchPanelToggle] = useToggle(false)
+    const searchEl = ref(null)
+    onClickOutside(searchEl, (event) => searchPanelState.value = false)
+</script>
+
+<style scoped>
+.search-panel-enter-active {
+  transition: all 0.15s ease-out;
+}
+
+.search-panel-leave-active {
+  transition: all 0.15s ease-in;
+}
+
+.search-panel-enter-from,
+.search-panel-leave-to {
+  transform: translateY(20px);
+  opacity: 0;
+}
+</style>
