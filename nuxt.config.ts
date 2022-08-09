7e7d3e19056797b0efa207975d72df9c2796d0ca
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-08-09 23:40:21 +0300

    qwd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,6 @@
 import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 
-//requiring path and fs modules
-const path = require('path');
-const fs = require('fs');
-//joining path of directory 
-const directoryPath = path.join(__dirname, '.nuxt');
-//passsing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        console.log(file); 
-    });
-    const data = fs.readFileSync(path.join(directoryPath, 'imports.d.ts'), 'utf8')
-    console.log(data)
-});
-
-
 export default defineNuxtConfig({
     isProduction: true,
     ssr: false,
