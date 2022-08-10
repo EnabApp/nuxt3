3e490c9619e45a7aea216f2b4c164779f2fd48d2
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-08-10 21:00:16 +0300

    ضصث

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
     modules: [
 
         /**˅˅˅˅ APPS ˅˅˅˅**/
-        // '@enab/todo',
+        '@enab/todo',
         '@enab/appstore',
         /**˄˄˄˄ APPS ˄˄˄˄**/
 
