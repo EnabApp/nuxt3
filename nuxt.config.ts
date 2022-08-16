52ae18e3be6e4c1e848ae0608ab30ef8acdace95
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-08-16 15:12:39 +0300

    qsad

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 
 export default defineNuxtConfig({
-    isProduction: true,
+    // isProduction: true,
     ssr: false,
     
     autoImports: {
@@ -22,11 +22,13 @@ export default defineNuxtConfig({
         /**˅˅˅˅ APPS ˅˅˅˅**/
         '@enab/todo',
         '@enab/appstore',
+        '@enab/recharge',
+        '@enab/calculator',
         /**˄˄˄˄ APPS ˄˄˄˄**/
-
+        
         '@enab/core',
-
         '@enab/ui',
+        
 
         '@nuxtjs/supabase',
 
