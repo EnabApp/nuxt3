8076aec88a47cc5adfcbd3ca85116c69437498de
Hasan Alkarnosy <gruceing@gmail.com>
2022-09-10 11:58:43 +0300

    update enab

---
+++ scripts/update-enab.mjs
@@ -0,0 +1,13 @@
+import { readdir } from 'fs/promises'
+import { execSync } from 'child_process'
+
+const getDirectories = async source =>
+  (await readdir(source, { withFileTypes: true }))
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => `${source}/${dirent.name}/`)
+
+const modulesPaths = await getDirectories('./modules')
+
+modulesPaths.forEach(async modulePath => {
+  execSync(`cd ${modulePath} && git pull && yarn install && yarn dev:prepare`, { stdio: 'inherit' })
+})
