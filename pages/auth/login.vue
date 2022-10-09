b7e5631b7ac188d8c545f0af9b00dc4838f0845a
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-10-10 01:05:01 +0300

    q

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,18 +1,17 @@
 <template>
-  <div bg="secondary dark:secondaryOp" h="screen" w="screen" flex="~ col" items="center" justify="center">
-    <div w="content" h="content" p="50px" border="rounded-10px" bg="primary dark:primaryOp" flex="~ col gap-45px"
-      items="center">
-      
-        <IconEnab text="primary" w="128px sm:64px md:96px" h="128px sm:64px md:96px" />
-      
+  <div h="screen" w="screen" flex="~ col" items="center" justify="center">
+    <div w="content" h="content" p="50px" border="rounded-10px" bg="primary dark:primaryOp" flex="~ col gap-45px" items="center">
+
+      <IconEnabSymbol text="primary" w="128px sm:64px md:96px" h="128px sm:64px md:96px" />
+
       <div flex="~ col gap-30px" justify="center" items="center">
-        <div flex="~ col gap-25px">
+        <div flex="~ col gap-4">
           <!-- Email -->
-          <UiInput v-model="input.email" w="270px" placeholder="البريد الإلكتروني" type="email" icon="IconEmail" />
+          <UiInput v-model="input.email" size="md" w="270px" placeholder="البريد الإلكتروني" type="email" icon="IconEmail" />
 
           <div flex="~ col gap-5px">
             <!-- Password -->
-            <UiInput v-model="input.password" w="270px" placeholder="كلمة المرور" type="password" icon="IconLock" />
+            <UiInput v-model="input.password" size="md" w="270px" placeholder="كلمة المرور" type="password" icon="IconLock" />
             <p v-if="authError" text="xs red" mr="10px" truncate="~" w="50">
               {{ authError }}
             </p>
@@ -20,27 +19,21 @@
 
           <!-- Submit -->
           <div v-if="input.email" class="delay-50 duration-200 animate-fade-in">
-            <UiButton @click="handelSubmit()">
-              <div flex="~ gap-15px" justify="center" text="primary dark:primaryOp" items="center">
+            <UiButton size="md" w="full" @click="handelSubmit()">
+              <div w="full" flex="~ gap-15px" justify="center" text="primary dark:primaryOp" items="center">
                 <span>تسجل الدخول</span>
-                <IconLogin w="20px" />
+                <!-- <IconLogin w="20px" /> -->
               </div>
             </UiButton>
           </div>
         </div>
 
         <!-- Divider -->
-        <div flex="~" w="full" justify="center" items="center">
-          <div w="33%" rounded="full" h="0.5px" bg="secondaryOp dark:secondary" />
-          <span>او سجل مع</span>
-          <div w="33%" rounded="full" h="0.5px" bg="secondaryOp dark:secondary" />
-        </div>
+        <div w="full" rounded="full" h="1px" bg="tertiaryOp dark:tertiary" />
         <div>
           <div flex="~ col gap-8px">
             <!-- Google Login -->
-            <div bg="primaryOp dark:primary" hover="secondaryOp dark:bg-secondary" duration="200" rounded="5px"
-              w="270px" h="50px" flex="~ gap-10px" justify="center" items="center" cursor="pointer"
-              @submit.prevent="signInWithGoogle()">
+            <div bg="primaryOp dark:primary" hover="secondaryOp dark:bg-secondary" duration="200" rounded="10px" w="270px" h="50px" flex="~ gap-10px" justify="center" items="center" cursor="pointer" @submit.prevent="signInWithGoogle()">
               <span text="primary dark:primaryOp" font="semibold">Google</span>
               <IconGoogle w="20px" h="20px" text="primary dark:primaryOp" />
             </div>
@@ -64,14 +57,12 @@
       </div>
     </div>
 
-    <div flex="~ gap-50px" h="48px" w="sm md:2xl lg:3xl" border="rounded-10px" m="10" justify="center" items="center"
-      bg="primary dark:primaryOp opacity-50 dark:opacity-50" un-text="primaryOp dark:primary xs md:sm lg:sm xl:md">
+    <div flex="~ gap-50px" h="48px" w="sm md:2xl lg:3xl" border="rounded-10px" m="10" justify="center" items="center" bg="primary dark:primaryOp opacity-50 dark:opacity-50" un-text="primaryOp dark:primary xs md:sm lg:sm xl:md">
       <span cursor="pointer">هل تحتاج المساعدة؟</span>
       <span cursor="pointer">نسيت كلمة المرور؟</span>
-      <nuxt-link to="/auth/register">
-        <span text="primaryOp dark:primary" underline="~" duration="200" decoration="blue-500 hover:red"
-          cursor="pointer">أليس لديك حساب؟</span>
-      </nuxt-link>
+      <NuxtLink to="/auth/register">
+        <span text="primaryOp dark:primary" underline="~" duration="200" decoration="blue-500 hover:red" cursor="pointer">أليس لديك حساب؟</span>
+      </NuxtLink>
     </div>
   </div>
 </template>
@@ -106,7 +97,7 @@ const handelSubmit = async () => {
     await login({ email: input.email, password: input.password });
     input.email = "";
     input.password = "";
-    
+
   } catch (err) {
     authError.value = err.message;
   }
