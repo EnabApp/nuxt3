d1bac73e3003ef7f42a09857802f2ecb545dc844
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-10-11 00:37:38 +0300

    Create index.post.ts

---
+++ server/api/data-unit/index.post.ts
@@ -0,0 +1,28 @@
+import { dataUnitModel } from "~~/schemas/dataUnit/DataUnit";
+import { sendError } from "h3";
+
+export default defineEventHandler(async (event) => {
+    try {
+        const { name, board_id, points } = await useBody(event);
+        if (!name) return { error: "name is required" };
+        if (!board_id) return { error: "board_id is required" };
+        if (!points) return { error: "points is required" };
+        const dataUnit = new dataUnitModel({
+            name,
+            board: board_id,
+            points,
+        });
+        await dataUnit.save();
+        return dataUnit;
+    }
+    catch (err) {
+        return sendError(
+            event,
+            createError({
+                statusCode: 400,
+                statusMessage: err.message,
+            })
+        );
+    }
+});
+
