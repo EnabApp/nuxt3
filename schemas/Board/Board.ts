c410c3cd794b3896384f488d27e0fe065896352d
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-10-10 18:04:36 +0300

    Update Board.ts

--- schemas/Board/Board.ts
+++ schemas/Board/Board.ts
@@ -48,7 +48,110 @@ const boardSchema = new Schema(
                 mobile: [responsiveSchema],
             },
             required: true,
-        }
+        },
+        dataUnit:[
+            {
+                type: Schema.Types.ObjectId,
+                ref: "DataUnit",
+            }
+        ]
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const boardModel = mongoose.model("Board", boardSchema);
+
+// Language: typescript
+// Path: schemas\business\Business.ts
+// Compare this snippet from schemas\dataUnit\DataUnit.ts:
+// import mongoose from "mongoose";
+// const { Schema } = mongoose;
+// 
+// const dataUnitSchema = new Schema(
+//     {
+//         name: {
+//             type: String,
+//             required: true,
+//         },
+//         points: {
+//             type: Number,
+//             required: true,
+//         },
+//         board: {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "Board",
+//             required: true,
+//         },
+//     },
+//     {
+//         timestamps: true,
+//     }
+// );
+// 
+// export const dataUnitModel = mongoose.model("DataUnit", dataUnitSchema);
+// Compare this snippet from schemas\space\Space.ts:
+// import mongoose from "mongoose";
+// const { Schema } = mongoose;
+// 
+// const spaceSchema = new Schema(
+//     {
+//         name: {
+//             type: String,
+//             required: true,
+//         },
+//         description: {
+//             type: String,
+//         },
+//         business: {
+//             type: Schema.Types.ObjectId,
+//             ref: "Business",
+//             required: true,
+//         },
+//         boards: [
+//             {
+//                 type: Schema.Types.ObjectId,
+//                 ref: "Board",
+//             },
+//         ],
+//         is_active: {
+//             type: Boolean,
+//             default: true,
+//         },
+//     },
+//     {
+//         timestamps: true,
+//     }
+// );
+// 
+// export const spaceModel = mongoose.model("Space", spaceSchema);
+// 
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const businessSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: false
+        },
+        is_active: {
+            type: Boolean,
+            default: true
+        },
+        spaces: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Space",
+                required: true
+            }
+        ]
+        ]
 
     }
 );
