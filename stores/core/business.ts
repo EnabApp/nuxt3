3b7bfb4f7309c68e19f0a4c75734530ce57db721
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-10-10 15:04:14 +0300

    Update business.ts

--- stores/core/business.ts
+++ stores/core/business.ts
@@ -1,8 +1,4 @@
 import { acceptHMRUpdate, defineStore } from "pinia";
-import { useFetch } from "@vueuse/core";
-import { BusinessType } from "~~/models/Business";
-import { UnitType } from "~~/models/Unit";
-import { ResposnivesType } from "~~/models/Board";
 import { RespType } from "~~/types/Response";
 
 export const useBusiness = defineStore("businessStore", {
@@ -12,23 +8,6 @@ export const useBusiness = defineStore("businessStore", {
 
   getters: {
     getBusinesses: (state) => state.businesses,
-    getBusinessesAsUnits: (state) => {
-      const units: any = []
-      state.businesses.map((business: BusinessType) => {
-        units.push({
-          id: business.id,
-          colSpan: 1,
-          rowSpan: 1,
-          componentName: 'DefaultContainer',
-          componentData: {...business, route: `boards/${business.id}`},          
-        })
-      })
-      return {
-        desktop: units as UnitType[],
-        tablet: units as UnitType[],
-        mobile: units as UnitType[],
-      } as ResposnivesType;
-    }
   },
 
   actions: {
@@ -38,6 +17,8 @@ export const useBusiness = defineStore("businessStore", {
         () => $fetch('/api/business')
       ) as RespType
       if (error.value) { console.log(error); return false}
+      console.log(data)
+      console.log(error)
       this.businesses = data.value?.data
     }
   }
