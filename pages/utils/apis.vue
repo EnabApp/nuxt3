b177d550a1443e8cb209445d17ae69431d36f938
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-10-12 20:03:39 +0300

    Update apis.vue

--- pages/utils/apis.vue
+++ pages/utils/apis.vue
@@ -1,12 +1,17 @@
 <!-- SHOW SPACES FOR A BUSINESS -->
 
 <template>
-    <div flex="~ col" w="full" m="8">
-        asd
+    <div flex="~ col gap-2" items="center" justify="center" w="full" m="8">
+        <div v-for="hook in allHooks" :key="hook.hook" flex="~" p="2" justify="between" rounded="lg" bg="secondary dark:secondaryOp dark:hover:opacity-70" w="3xl">
+            <span>{{ hook.hook }}</span>
+            <span>{{ hook.hook.split(':').shift() }}</span>
+            <div v-for="attr in hook?.data" :key="attr">
+                <span>{{ attr }}</span>
+            </div>
+        </div>        
     </div>
 </template>
   
 <script setup>
-
 </script>
   
