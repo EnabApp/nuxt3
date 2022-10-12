c16d94f7eafcf019d6738804371631e3194cb2d7
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-10-12 20:21:29 +0300

    Update hooksCore.ts

--- composables/shared/api/hooks/hooksCore.ts
+++ composables/shared/api/hooks/hooksCore.ts
@@ -2,9 +2,9 @@ export default [
   { hook: "post:login", endpoint: "auth/login" },
   { hook: "post:register", endpoint: "auth/register" },
   { hook: "get:business", endpoint: "core/business" },
-  { hook: "post:business", endpoint: "core/business", data: [{ name: "string" }, { user_id: "string" }, { category_id: "string" }, { address: "string" }] },
+  { hook: "post:business", endpoint: "core/business", data: [{ name: "name",type: "string" }, { name: "user_id",type: "string" }, { name: "category_id", type: "string" }, { name: "description", type: "string" }]},
   { hook: "get:space", endpoint: "core/space" },
-  { hook: "post:space", endpoint: "core/space", data: [{ name: "string" }, { business_id: "string" }, { description: "string" }] },
+  { hook: "post:space", endpoint: "core/space", data: [{ name: "name",type: "string" }, { name: "business_id",type: "string" }, { name: "category_id", type: "string" }]},
   { hook: "get:board", endpoint: "core/board" },
 
   {
