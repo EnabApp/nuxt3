48201db5485759fb48f84554c1a9a48577b6b0dd
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-10-12 19:41:39 +0300

    add

--- server/api/core/space/index.post.ts
+++ server/api/core/space/index.post.ts
@@ -1,16 +1,25 @@
 export default defineEventHandler(async (event) => {
   try {
     const { name, business_id, description } = await useBody(event);
-    if (!name) return { error: "name is required" };
-    if (!business_id) return { error: "business_id is required" };
+    if (!name || !business_id) {
+      return sendError(
+        event,
+        createError({
+          statusCode: 400,
+          statusMessage: "Invalid params",
+        })
+      );
+    }
 
-    const space = new spaceModel({
-      name,
-      business: business_id,
-      description: description,
-    });
-    await space.save();
-    return space;
+  const { inserSpace } = useSpace();
+  const space = await inserSpace({
+    name,
+    business_id,
+    description,
+  });
+  
+  return { space };
+
   } catch (err) {
     return sendError(
       event,
