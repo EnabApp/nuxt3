2301e21967b22d38028bd41e9e66e244268ffddc
Hassan Kadhim <66673458+HassanKadhim@users.noreply.github.com>
2022-10-12 20:43:50 +0300

    Update index.post.ts

--- server/api/core/board/index.post.ts
+++ server/api/core/board/index.post.ts
@@ -1,5 +1,28 @@
+import { sendError } from "h3";
+
 export default defineEventHandler(async (event) => {
-  return {
-    statusCode: 200,
-  };
+  const { insertBoard } = useBoard();
+
+  const { name, space_id, description } = await useBody(event);
+  try {
+    if (!name || !space_id) {
+      return sendError(
+        event,
+        createError({
+          statusCode: 400,
+          statusMessage: "Missing Required Fields",
+        })
+      );
+    }
+    const board = await insertBoard({ name, space_id, description });
+    return board;
+  } catch (err) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 400,
+        statusMessage: err.message,
+      })
+    );
+  }
 });
