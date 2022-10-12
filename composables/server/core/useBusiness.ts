a3b42296138f329a5902824e8d1382a9d1f8fb1a
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-10-12 16:55:22 +0300

    add

--- composables/server/core/useBusiness.ts
+++ composables/server/core/useBusiness.ts
@@ -1,16 +1,31 @@
+import { sendError } from "h3";
 export default () => {
   // Export Function to be used
-  //const FunctionName = () => {
-  //    return new Promise(async (resolve, reject) => {
-  //        try {
-  //            //Do Something
-  //            resolve();
-  //        } catch (err) {
-  //            reject(err);
-  //        }
-
-  //Return Function to be used
-  return {
-    //FunctionName,
+  const inserBusiness = ({ name, user_id, category_id, address }) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const business = new businessModel({
+          name: name,
+          users: [
+            {
+              user_id: user_id,
+              Permissions: ["owner"],
+            },
+          ],
+          categories: [category_id],
+          address: address,
+        });
+        await business.save();
+        resolve(business);
+      } catch (err) {
+        reject(err);
+      }
+    });
+    
+    //Return Function to be used
+    return {
+      inserBusiness,
+    };
   };
 };
+
