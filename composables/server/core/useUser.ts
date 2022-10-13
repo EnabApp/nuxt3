77580205ee261fbbeca15f697f1831a56995ee78
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-10-13 22:48:39 +0300

    add

--- composables/server/core/useUser.ts
+++ composables/server/core/useUser.ts
@@ -1,12 +1,13 @@
 export default () => {
   // Export Function to be used
+  const { userRefactor } = useRefactor();
 
   const getUserById = (user_id) => {
     return new Promise(async (resolve, reject) => {
       try {
         const data = await userModel
           .findOne({ _id: user_id })
-          .populate({ path: "profile", model: profileModel });
+          .populate({path: "profile", model: profileModel})
         resolve(data);
       } catch (err) {
         reject(err);
