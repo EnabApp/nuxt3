3cadd6b1abda25376d0a62fc1a6acb063c75de13
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-10-15 13:02:28 +0300

    Update index.put.ts

--- server/api/core/business/index.put.ts
+++ server/api/core/business/index.put.ts
@@ -1,14 +1,14 @@
 import { sendError } from "h3";
 export default defineEventHandler(async (event) => {
-    const { id, name, address, users, categories, spaces, is_active } = await useBody(event);
+    const { id, name, address, user_id, category_id } = await useBody(event);
     const { updateBusiness } = useBusiness();
     try {
-        if (!id || !name || !address || !users || !categories || !spaces || !is_active)
+        if (!id || !name || !address || !user_id || !category_id )
             return sendError(
                 event,
                 createError({
                     statusCode: 400,
-                    statusMessage: "id, name, address, user_id, category_id",
+                    statusMessage: "id, name, address, user_id, category_id required",
                 })
             );
         return await updateBusiness({ id, name, address, user_id, category_id });
