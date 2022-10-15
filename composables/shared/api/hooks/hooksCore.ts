579457a0e61438d243f9810c814af0db2955cd31
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-10-15 13:26:46 +0300

    qwd

--- composables/shared/api/hooks/hooksCore.ts
+++ composables/shared/api/hooks/hooksCore.ts
@@ -1,23 +1,36 @@
 export default [
+  // Auth
   { hook: "post:login", endpoint: "auth/login", data: [{ name: "email" }, { name: "password" }] },
   { hook: "post:register", endpoint: "auth/register", data: [{ name: "email" }, { name: "password" }, { name: "name" }] },
+
+  // User
   { hook: "get:user", endpoint: "core/user", data: [{ name: "user_id" }] },
+
+  // Business
   { hook: "get:business", endpoint: "core/business" },
   { hook: "post:business", endpoint: "core/business", data: [{ name: "name" }, { name: "user_id" }, { name: "category_id" }, { name: "description" }] },
   { hook: "delete:business", endpoint: "core/business", data: [{ name: "business_id" }] },
+
+  // Business Categories
   { hook: "get:business-category", endpoint: "core/business-category" },
   { hook: "post:business-category", endpoint: "core/business-category", data: [{ name: "name" }] },
   { hook: "delete:business-category", endpoint: "core/business-category", data: [{ name: "category_id" }] },
+
+  // Space
   { hook: "get:spaces", endpoint: "core/space" },
   { hook: "post:space", endpoint: "core/space", data: [{ name: "name" }, { name: "business_id" }, { name: "category_id" }] },
   { hook: "delete:space", endpoint: "core/space", data: [{ name: "space_id" }] },
   { hook: "get:space", endpoint: "core/space", data: [{ name: "space_id" }] },
   { hook: "get:spaces-business", endpoint: "core/space/by-business", data: [{ name: "business_id" }] },
+
+  // Board
   { hook: "get:board", endpoint: "core/board" },
   { hook: "get:board", endpoint: "core/board", data: [{ name: "board_id" }] },
   { hook: "post:board", endpoint: "core/board", data: [{ name: "name" }, { name: "space_id" }, { name: "description" }] },
   { hook: "delete:board", endpoint: "core/board", data: [{ name: "board_id" }] },
   { hook: "get:board", endpoint: "core/board" , data: [{ name: "board_id" }]}, 
+
+  // Permission
   { hook: "post:permission", endpoint: "core/permission", data: [{ name: "business_id" }, { name: "client_id" }, { name: "ref" }, { name: "type" }]},
   { hook: "get:permission", endpoint: "core/permission" },
   { hook: "get:permission-business", endpoint: "core/permission/by-business", data: [{ name: "business_id" }]},
