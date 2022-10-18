5c1e9bd100489f13740484874f51a2f003107076
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-10-18 19:38:40 +0300

    test

---
+++ pages/domain/[id].vue
@@ -0,0 +1,10 @@
+<template>
+    <NuxtLayout name="user">
+        <span>Hello World! - {{ id }}</span>
+    </NuxtLayout>
+</template>
+
+<script setup>
+const { params } = useRoute();
+const { id } = params;
+</script>
