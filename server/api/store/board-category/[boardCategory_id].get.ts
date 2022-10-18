c8e8966a16154cbcc0522fa8f8766df3751e2373
Mohammed Jawad <jawad6686@gmail.com>
2022-10-18 18:42:38 +0300

    add

---
+++ server/api/store/board-category/[boardCategory_id].get.ts
@@ -0,0 +1,21 @@
+import { sendError } from "h3";
+
+export default defineEventHandler(async (event) => {
+    try{
+        const boardCategory_id = await event.context.params.boardCategory_id;
+        if (!boardCategory_id) {
+            return sendError(event, createError({
+                statusCode: 400,
+                statusMessage: "boardCategory_id is required"
+            }))
+        }
+        const { getBoardCategoryById } = useBoardCategory();
+        return await getBoardCategoryById(boardCategory_id);
+    }
+    catch (err) {
+        return sendError(event, createError({
+            statusCode: 400,
+            statusMessage: err.message
+        }))
+    }
+});
