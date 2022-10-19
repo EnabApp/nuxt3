bb06f1e929f02db634248ccac1a5ed58670e502e
Mohammed Jawad <jawad6686@gmail.com>
2022-10-19 19:02:57 +0300

    add

--- schemas/core/user/Membership.ts
+++ schemas/core/user/Membership.ts
@@ -13,6 +13,10 @@ const membershipSchema = new Schema(
                 ref: "User",
             },
         ],
+        points: {
+            type: Number,
+            default: 0,
+        },
         is_active: {
             type: Boolean,
             default: true,
@@ -22,3 +26,24 @@ const membershipSchema = new Schema(
         timestamps: true,
     }
 );
+
+const Membership = mongoose.model("Membership", membershipSchema);
+
+const membershipRefactor = (data) => {
+    return {
+        id: data?._id,
+        name: data?.name,
+        users: data?.users.map((user) => {
+            return {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+            };
+        }),
+        points: data?.points,
+        is_active: data?.is_active,
+    };
+
+};
+
+export { Membership, membershipRefactor };
---
+++ server/api/store/sales/buyBoard.post.ts
@@ -0,0 +1,25 @@
+import { sendError } from "h3";
+
+export default defineEventHandler(async (event) => {
+    const { board_id, user_id } = await useBody(event);
+    const { buyBoard } = useSales();
+    try {
+        if (!board_id || !user_id)
+            return sendError(
+                event,
+                createError({
+                    statusCode: 400,
+                    statusMessage: "board_id and user_id are required",
+                })
+            );
+        return await buyBoard({ board_id, user_id });
+    } catch (err) {
+        return sendError(
+            event,
+            createError({
+                statusCode: 400,
+                statusMessage: err.message,
+            })
+        );
+    }
+});
