186e4e8178484c50dccbf20bac09933ffd4c091e
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-10-20 16:55:44 +0300

    Update useSales.ts

--- composables/server/store/useSales.ts
+++ composables/server/store/useSales.ts
@@ -8,7 +8,7 @@ export default () => {
     //             if (!board) {
     //                 reject("Board not found");
     //             }
-                
+
     //             const user = await userModel.findByIdAndUpdate(user_id, {
     //                 $push: { boards: board_id },
     //                 }, { new: true });
@@ -23,88 +23,85 @@ export default () => {
     // };
 
     //buy board method
-const buyBoard = (date) => {
-    const { board_id, user_id} = date;
-    return new Promise(async (resolve, reject) => {
-        try{
-        
-            const board = await boardModel.find({_id:board_id});
-            if (!board) {
-                reject("Board not found");
+    const buyBoard = (date) => {
+        const { board_id, user_id } = date;
+        return new Promise(async (resolve, reject) => {
+            try {
+
+                const board = await boardModel.find({ _id: board_id });
+                if (!board) {
+                    reject("Board not found");
+                }
+
+                const user = await userModel.find({ _id: user_id });
+                if (!user) {
+                    reject("User not found");
+                }
+
+                //if user already have the board
+                if (user.boards.find(({ board }) => board == board_id)) {
+                    reject("User already have this board");
+                }
+
+                //get userPoints from profile
+                const profile = await profileModel.findOne({ user: user_id });
+                const userPoints = profile.points;
+
+                //get boardPoints
+                const boardPoints = board.points;
+
+                //cut points from user if has enough points
+                if (userPoints >= boardPoints) {
+                    profile.points -= boardPoints;
+                    await profile.save();
+                }
+                else {
+                    reject("User has not enough points");
+                }
+
+                //push board to user 
+                user.boards.push({
+                    board: board._id
+                });
+
+                await user.save();
+                resolve("Board bought");
+
+            } catch (err) {
+                reject(err);
             }
-
-            const user = await userModel.find({_id:user_id});
-            if (!user) {
-                reject("User not found");
-            }
-
-            //if user already have the board
-            if(user.boards.find(({board}) => board == board_id)){
-                reject("User already have this board");
-            }
-
-            //get userPoints from profile
-            const profile = await profileModel.findOne({user: user_id});
-            const userPoints = profile.points;
-            
-            //get boardPoints
-            const boardPoints = board.points;
-
-            //cut points from user if has enough points
-            if(userPoints >= boardPoints)
-            {
-                profile.points -= boardPoints;
-                await profile.save();
-            }
-            else {
-                reject("User has not enough points");
-            }
-
-            //push board to user 
-            user.boards.push({
-                board: board._id
-            });
-
-            await user.save();
-            resolve("Board bought");
-
-        } catch (err)
-        {
-            reject(err);
-        }
-    });
-};
+        });
+    };
 
 
     // buy pack method
     const buyPack = ({ pack_id, user_id }) => {
-        
+
         return new Promise(async (resolve, reject) => {
             try {
-                const pack = await packModel.find({_id:pack_id});
+                const pack = await packModel.find({ _id: pack_id });
                 if (!pack) {
                     reject("Pack not found");
                 }
-                const user = await userModel.find({_id:user_id});
+                const user = await userModel.find({ _id: user_id });
                 if (!user) {
                     reject("User not found");
                 }
 
                 //if user already have the pack
-                if(user.packs.find(({pack}) => pack == pack_id)){
+                if (user.packs.find(({ pack }) => pack == pack_id)) {
                     reject("User already have this pack");
                 }
-                
+
                 //get userPoints from profile
-                const profile = await profileModel.findOne({user: user_id});
+                const profile = await profileModel.findOne({ user: user_id });
                 const userPoints = profile.points;
 
                 //get packPoints
                 const packPoints = pack.points;
 
                 //cut points from user if has enough points
-                if(userPoints >= packPoints)
-                {
+                if (userPoints >= packPoints) {
                     profile.points -= packPoints;
                     await profile.save();
                 }
@@ -113,9 +110,10 @@ const buyBoard = (date) => {
                 }
 
                 //push pack to user
-                user.packs.push({
-                    pack: pack._id
-                });
+                user.packs.findByIdAndUpdate(user_id, {
+                    $push: { packs: pack_id },
+                }, { new: true });
+                
 
                 await user.save();
                 resolve("Pack bought");
