c0678f8af15d6d63de7d7257b281214f35e5006e
Hussein Najah <husseinnajah123@gmail.com>
2022-10-21 23:23:12 +0300

    Register

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,26 +1,34 @@
 <template>
   <div h="screen" w="screen" flex="~ col" justify="center" items="center" mt="100px">
-    <div w="content" h="content" border="rounded-10px" bg="primary dark:primaryOp" flex="~ col gap-66px" items="center" relative="~">
+    <div w="content" h="content" border="rounded-10px" bg="primary dark:primaryOp" flex="~ col gap-66px" items="center"
+      relative="~">
       <!-- ?Logo -->
       <IconEnabLight w="112px md:128px" bottom="105% md:120%" absolute="~" />
-
       <div flex="~ col gap-10px md:gap-16px" justify="center" w="100%" h="100%" items="center">
-        <AuthSginUpForm />
+        <Transition name="slide-up">
+          <AuthSginUpForm v-if="registerState" />
+        </Transition>
         <!-- ?Error Message -->
         <p v-if="authStore.getCreateError" w="full" text="xs red right" mr="10px">
           {{ authStore.getCreateError }}
         </p>
 
         <!-- ?Submit -->
-        <div @click="authStore.register()" text="center lg primary dark:primaryOp" h="50px" position="relative" bg="primaryOp dark:primary" hover="secondaryOp dark:bg-secondary" w="190px lg:270px" duration="200" rounded="10px" flex="~" justify="center" items="center" cursor="pointer">
-          <span>انشاء الحساب</span>
-          <IconRegister v-if="!loading" right="4" position="absolute" w="22px" text="primary dark:primaryOp" />
-          <IconLoading v-else right="4" position="absolute" w="22px" text="primary dark:primaryOp" />
+        <div key="LoginWithEmailButton" @click="registerState ? authStore.register() : registerToggle()"
+          text="center lg primary dark:primaryOp" h="50px" position="relative" bg="primaryOp dark:primary"
+          hover="secondaryOp dark:bg-secondary" w="190px lg:270px" duration="200" rounded="10px" flex="~"
+          justify="center" items="center" cursor="pointer">
+          <span v-if="registerState">التسجيل</span>
+          <span v-else>انشاء حسابك</span>
+          <IconRegister v-if="registerState" right="4" position="absolute" w="22px" text="primary dark:primaryOp" />
+          <IconEmailPlus v-else right="4" position="absolute" w="22px" text="primary dark:primaryOp" />
         </div>
       </div>
 
       <!-- ?Feed Back -->
-      <div flex="~ gap-10px md:gap-50px" h="48px" w="xs md:xl lg:xl" border="rounded-10px" m="10" justify="center" items="center" bg="primary dark:primaryOp opacity-50 dark:opacity-50" text="primaryOp dark:primary 12px md:xs lg:sm xl:md">
+      <div flex="~ gap-10px md:gap-50px" h="48px" w="xs md:xl lg:xl" border="rounded-10px" m="10" justify="center"
+        items="center" bg="primary dark:primaryOp opacity-50 dark:opacity-50"
+        text="primaryOp dark:primary 12px md:xs lg:sm xl:md">
         <span cursor="pointer">هل تحتاج المساعدة؟</span>
         <nuxt-link decoration="none" to="/auth/login">
           <span text="primaryOp dark:primary" cursor="pointer">تمتلك حساب؟</span>
@@ -34,14 +42,13 @@
 definePageMeta({
   title: "Register",
   middleware: "guest",
-
 });
+const [registerState, registerToggle] = useToggle();
 
 const authStore = useAuth();
-const router = useRouter();
-const authError = ref("");
 const loading = ref(false);
-
+// const router = useRouter();
+// const authError = ref("");
 
 // Register
 // const register = async () => {
